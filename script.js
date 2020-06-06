64762a89607c0d9b2df7e86e911109de65efba3c
Likhita-11 <62496768+Likhita-11@users.noreply.github.com>
2020-06-06 16:47:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ['Ajay','Niranjan','Likitha','siddu','japan','india','jaggu','Australia','jambu','jpriya','darliki'];
+    var names = ['Ajay','Niranjan','Likitha','siddu','japan','india','jaggu','Australia','jambu','jpriya','darliki '];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
